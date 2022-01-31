079163957f5a377026421345c4703e1ad34b5b91
Gabriel Paz <fgpaz@live.com>
2022-01-31 11:13:17 -0300

    Commit inicial

---
+++ PruebaAPI/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaAPI.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    }
+}
